2ad53466f78b30d85b2ae4ebc07ea67d1ad7c8fd
bhasfe <barishasdemir@gmail.com>
2021-01-31 21:56:28 +0200

    solution

--- src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
+++ src/main/scala/com/trendyol/bootcamp/homework/ProductMergerJob.scala
@@ -1,21 +1,75 @@
 package com.trendyol.bootcamp.homework
 
+import org.apache.spark.sql.catalyst.expressions.UnixTimestamp
+import org.apache.spark.sql.{DataFrame, Encoders, SaveMode, SparkSession}
+import org.apache.spark.sql.expressions.{UserDefinedFunction, Window}
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+
+import scala.util.Try
+
 object ProductMergerJob {
 
   def main(args: Array[String]): Unit = {
 
-    /**
-    * Find the latest version of each product in every run, and save it as snapshot.
-    *
-    * Product data stored under the data/homework folder.
-    * Read data/homework/initial_data.json for the first run.
-    * Read data/homework/cdc_data.json for the nex runs.
-    *
-    * Save results as json, parquet or etc.
-    *
-    * Note: You can use SQL, dataframe or dataset APIs, but type safe implementation is recommended.
-    */
+    val spark = SparkSession
+      .builder()
+      .master("local")
+      .appName("Trendyol Week5 Homework")
+      .getOrCreate()
 
+    import spark.implicits._
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val schema = StructType(
+      List(
+        StructField("id", IntegerType),
+        StructField("name", StringType),
+        StructField("category", StringType),
+        StructField("brand", StringType),
+        StructField("color", StringType),
+        StructField("price", FloatType),
+        StructField("timestamp", LongType)
+      )
+    )
+
+    val upToDateData = Try(
+      spark.read
+        .schema(schema)
+        .json("data/homework/uptodate_data.json")
+        .withColumn("timestamp", from_unixtime($"timestamp" / 1000))
+    ).getOrElse(spark.createDataFrame(spark.sparkContext.emptyRDD[Row], schema))
+
+    val path = {
+      if (upToDateData.count()==0){
+        "data/homework/initial_data.json"
+      } else {
+        "data/homework/cdc_data.json"
+      }
+    }
+    val cdcData = spark.read
+      .schema(schema)
+      .json(path)
+      .withColumn("timestamp", from_unixtime($"timestamp" / 1000))
+
+    // upToDateData.show()
+    // cdcData.show()
+
+    val rankWindow = Window.partitionBy("id").orderBy(col("timestamp").desc)
+
+    val target = upToDateData.union(cdcData)
+      .withColumn("rownum", row_number().over(rankWindow))
+      .filter(col("rownum") === 1)
+      .drop("rownum")
+      .orderBy("id")
+      .repartition(1)
+      .write
+      .json("data/homework/uptodate_data.json")
+
+
+
+    // target.show()
   }
-
 }
